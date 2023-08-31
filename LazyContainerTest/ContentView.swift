6c6b4bce6f7c8b24b4506dd8de198d4f87d3f831
sclo <sclo@gms.ndhu.edu.tw>
2023-08-31 16:32:34 +0800

    Initial Commit

---
+++ LazyContainerTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LazyContainerTest
+//
+//  Created by 羅壽之 on 2023/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    var body: some View {
+       Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
